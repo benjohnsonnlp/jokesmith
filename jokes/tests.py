7a04e4b12d2c2a60ef6c3bd1fde8c7a2fcc312f2
Ben <doc.ben.johnson@gmail.com>
2020-04-26 17:26:47 -0400

    tests ready

--- jokes/tests.py
+++ jokes/tests.py
@@ -39,3 +39,34 @@ class WorkflowTests(TestCase):
         elem = browser.find_element_by_css_selector('.card-body')
         self.assertIsNotNone(elem)
 
+    def test_ready(self):
+        browsers = [
+            webdriver.Chrome(),
+            webdriver.Chrome(),
+            webdriver.Chrome(),
+        ]
+
+        url = "http://localhost:8000" + reverse('index')
+        for i, browser in enumerate(browsers):
+            browser.get(url)
+
+            elem = browser.find_element_by_id('username')  # Find the search box
+            elem.send_keys('testuser' + str(i) + Keys.RETURN)
+
+            elem = browser.find_element_by_id('session_name')
+            elem.send_keys('testsession' + Keys.RETURN)
+
+
+        time.sleep(1)
+        for browser in browsers:
+            button = browser.find_element_by_id('readyButton')
+            button.click()
+
+        time.sleep(1)
+        browser = browsers[0]
+        button = browser.find_element_by_id('readyButton')
+        self.assertFalse(button.is_displayed())
+
+
+
+
